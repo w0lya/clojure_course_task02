5c3bc305712cc5eca101418f8c38b43cc9e6bf80
Volha Samusik <w0lya.s@gmail.com>
2013-03-28 01:23:22 +0300

    seems to be working OK

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,14 @@
 (ns clojure-course-task02.core
   (:gen-class))
 
-
+;not exactly 'parallel threads for each child dir', but does the required job anyway...
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (let [file-name-pattern (re-pattern file-name)]
+  (->> path
+      (clojure.java.io/file)
+      (file-seq)
+      (pmap #(.getName %))
+      (filter (partial re-matches file-name-pattern)))))  
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -15,4 +19,5 @@
     (usage)
     (do
       (println "Searching for " file-name " in " path "...")
-      (dorun (map println (find-files file-name path))))))
+      (dorun (map println (find-files file-name path)))
+	  (shutdown-agents))))
